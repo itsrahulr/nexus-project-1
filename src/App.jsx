1d587f2d86564087ae718ff950f0c80f0178aa39
itsrahulr <therahul27r@gmail.com>
2024-03-07 16:40:49 +0530

    fix:app.js paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -6,13 +6,13 @@ import Signup from "./components/Signup";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route index path="/" element={<Login />} />
+          <Route index element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
